8ce2de1138db8574621a2f209e7404689bd5c624
Maxime <maxime.huynh@okolo.fr>
2025-05-02 16:48:08 +0200

    add style

--- snake.js
+++ snake.js
@@ -2,9 +2,15 @@ const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 const gridSize = 20;
 const tileCount = canvas.width / gridSize;
-const loop = setInterval(gameLoop, 100);
-
+const scoreDisplay = document.getElementById('score');
+const startButton = document.querySelector('.start');
+const modalContainer = document.querySelector('.modal_container');
+const pauseButton = document.querySelector('.pause');
+const newButton = document.querySelector('.new');
 let score = 0;
+let loop = null;
+let isPaused = false;
+
 let food = { x: Math.floor(Math.random() * tileCount), y: Math.floor(Math.random() * tileCount) };
 
 let snake = [{ x: 10, y: 10 }];
@@ -61,7 +67,8 @@ function gameLoop() {
   }
 
   if (eatFood(newHead)) {
-    score++;
+    score++
+    scoreDisplay.textContent = 'Score : ' + score
     spawnFood();
   } else {
     snake.pop(); //Removes the last element from an array and returns it
@@ -76,27 +83,69 @@ function draw() {
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   // Serpent vert (plus clair)
-  ctx.fillStyle = '#8750f7';
+  ctx.fillStyle = '#00FF00';
   for (let segment of snake) {
     ctx.fillRect(segment.x * gridSize, segment.y * gridSize, gridSize, gridSize);
   }
 
   // Pomme (carré inversé)
-  ctx.fillStyle = '#8750f76e';
+  ctx.fillStyle = '#00ff005e';
   ctx.fillRect(food.x * gridSize, food.y * gridSize, gridSize, gridSize);
 
-  // Texte score en pixel font
-  ctx.fillStyle = '#8750f7';
-  ctx.font = '12px monospace';
-  ctx.fillText('Score: ' + score, 10, 20);
+}
+
+function pause() {
+  if (loop !== null) {
+    clearInterval(loop);
+    loop = null;
+    isPaused = true;
+    console.log("Game paused");
+  }
+}
+function start() {
+  if (loop === null) {
+    loop = setInterval(gameLoop, 100);
+    isPaused = false;
+  }
+}
+
+function newGame() {
+  clearInterval(loop);
+  score = 0;
+  loop = null;
+  isPaused = false;
+  food = { x: Math.floor(Math.random() * tileCount), y: Math.floor(Math.random() * tileCount) };
+  snake = [{ x: 10, y: 10 }];
+  direction = 'right';
+  start();
 }
 
 
-// Direction sécurisée (évite de se retourner sur soi-même)
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'ArrowRight' && direction !== 'left') direction = 'right';
-  if (event.key === 'ArrowLeft' && direction !== 'right') direction = 'left';
-  if (event.key === 'ArrowUp' && direction !== 'down') direction = 'up';
-  if (event.key === 'ArrowDown' && direction !== 'up') direction = 'down';
+document.addEventListener('keydown', (e) => {
+  if (e.key === 'ArrowRight' && direction !== 'left') direction = 'right';
+  if (e.key === 'ArrowLeft' && direction !== 'right') direction = 'left';
+  if (e.key === 'ArrowUp' && direction !== 'down') direction = 'up';
+  if (e.key === 'ArrowDown' && direction !== 'up') direction = 'down';
+  if (e.key === "p" || e.key === "P") !isPaused && pause();
+  if (e.key === "s" || e.key === "S") isPaused && start();
 });
 
+startButton.addEventListener('click', () => {
+  modalContainer.classList.remove('active')
+  start();
+});
+
+newButton.addEventListener('click', () => {
+  modalContainer.classList.remove('active')
+  newGame();
+})
+
+pauseButton.addEventListener('click', () => {
+  if(isPaused) {
+    start();
+    pauseButton.textContent = "Pause"
+  } else {
+    pause();
+    pauseButton.textContent = "Start"
+  }
+})
